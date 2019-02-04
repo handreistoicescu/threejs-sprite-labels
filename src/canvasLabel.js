bcb260e0cdcff83906e606164fc89ad0f693cdff
Andrei Stoicescu <andrei.stoicescu@yopeso.com>
2019-02-04 16:57:29 +0200

    WIP

---
+++ src/canvasLabel.js
@@ -0,0 +1,99 @@
+import * as THREE from 'three';
+
+export default function addLabel(threeObj, text) {
+  if (!threeObj.isObject3D) {
+    throw "Object is not valid threeJS Object3D. Can't apply the label.";
+  }
+
+  const obj = threeObj;
+  obj.geometry.computeBoundingSphere();
+  const objBoundingSphereRadius = obj.geometry.boundingSphere.radius;
+
+  const label = getLabelSprite(
+    text,
+    48,
+    'white',
+    'black',
+    objBoundingSphereRadius
+  );
+  obj.add(label);
+
+  console.log(objBoundingSphereRadius);
+
+  return obj;
+}
+
+function getLabelSprite(
+  sText,
+  fontSize,
+  sColor,
+  sBgColor,
+  objContainingSphereRadius
+) {
+  const canvas = document.createElement('canvas');
+  const ctx = canvas.getContext('2d');
+  const fontStyles = `${fontSize}px sans-serif`;
+
+  // see how wide the text will be
+  ctx.font = fontStyles;
+  const textWidth = ctx.measureText(sText).width;
+
+  // compute a padding
+  const textBoxPadding = fontSize * 0.2;
+
+  // total dimensions of text box
+  const textBoxWidth = textWidth + 2 * textBoxPadding;
+  const textBoxHeight = fontSize + 2 * textBoxPadding;
+
+  // canvas height will be assigned from parameter
+  // canvas.height = 500;
+  canvas.height = objContainingSphereRadius * 6;
+
+  // offset is computed relative to height because of how the design looks like
+  const textBoxOffset = canvas.height / 4;
+  canvas.width = (textBoxWidth + textBoxOffset) * 2;
+  canvas.style.backgroundColor = "rgba(255, 255, 255, 0)";
+
+  console.log(
+    `Canvas width: ${canvas.width}\n`,
+    `Canvas height: ${canvas.height}\n`,
+    `Textbox width: ${textBoxWidth}\n`,
+    `Obj sphere radius: ${objContainingSphereRadius}\n`
+  );
+
+
+  // add the background of the label
+  ctx.fillStyle = sBgColor;
+  ctx.fillRect((canvas.width / 2 + textBoxOffset), 0, textBoxWidth, textBoxHeight);
+
+  ctx.fillStyle = sColor;
+
+  // set the font styles again, as now they have reset because of the canvas resizing
+  ctx.font = fontStyles;
+
+  ctx.fillText(sText, (canvas.width / 2 + textBoxOffset + textBoxPadding), (fontSize + textBoxPadding));
+
+
+  // draw a line from the bottom-center of the text to all the way down the canvas
+  ctx.fillStyle = 'gray';
+  // draw the underline
+  ctx.beginPath();
+  ctx.moveTo((canvas.width / 2 + textBoxOffset - textBoxPadding), textBoxHeight / 2);
+  ctx.lineTo(canvas.width / 2, canvas.height / 6);
+  ctx.lineTo(canvas.width / 2, canvas.height / 2);
+  ctx.stroke();
+
+  // build the ThreeJS sprite
+  return getSpriteObject(canvas);
+}
+
+function getSpriteObject(canvas) {
+  const spriteMaterial = new THREE.SpriteMaterial({
+    map: new THREE.CanvasTexture( canvas )
+  });
+  const sprite = new THREE.Sprite( spriteMaterial );
+
+  sprite.scale.set(1, 1, 1);
+
+  return sprite;
+}
--- src/index.js
+++ src/index.js
@@ -1,22 +1,13 @@
 import * as THREE from 'three';
-import printMe from './print.js';
+import addLabel from './canvasLabel';
 import PaperTexture from './paper.jpg';
 
-printMe();
-
-const canvasLabel = getCanvasLabel(
-  'Label text',
-  48,
-  'white',
-  'black',
-  80
-);
-
-document.documentElement.appendChild(canvasLabel);
+// document.documentElement.appendChild(canvasLabel);
 
 const scene = new THREE.Scene();
-scene.background = new THREE.Color( 0xffffff );
+scene.background = new THREE.Color( 'salmon' );
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+// const camera = new THREE.OrthographicCamera( window.innerWidth / - 2,  window.innerHeight / 2, window.innerHeight / 2, window.innerHeight / - 2, 1, 1000 );
 
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
@@ -37,15 +28,9 @@ const material = new THREE.MeshPhongMaterial( {
 } );
 
 const cube = new THREE.Mesh( geometry, material );
-scene.add( cube );
-
-const spriteMaterial = new THREE.SpriteMaterial({
-  map: new THREE.CanvasTexture( canvasLabel )
-});
-const sprite = new THREE.Sprite( spriteMaterial );
-sprite.scale.set(1, 1, 1);
-
-scene.add( sprite );
+console.log(cube);
+const cubeWithLabel = addLabel(cube, 'Label text');
+scene.add( cubeWithLabel );
 
 camera.position.z = 5;
 
@@ -56,58 +41,3 @@ function animate() {
 	renderer.render( scene, camera );
 }
 animate();
-
-
-function getCanvasLabel(sText, fontSize, sColor, sBgColor, objContainingSphereRadius) {
-  const canvas = document.createElement('canvas');
-  const ctx = canvas.getContext('2d');
-  const fontStyles = `${fontSize}px sans-serif`;
-
-  // see how wide the text will be
-  ctx.font = fontStyles;
-  const textWidth = ctx.measureText(sText).width;
-
-  // compute a padding
-  const textBoxPadding = fontSize * 0.2;
-
-  // total dimensions of text box
-  const textBoxWidth = textWidth + 2 * textBoxPadding;
-  const textBoxHeight = fontSize + 2 * textBoxPadding;
-
-  // canvas height will be assigned from parameter
-  // canvas.height = 500;
-  canvas.height = objContainingSphereRadius * 6;
-
-  // offset is computed relative to height because of how the design looks like
-  const textBoxOffset = canvas.height / 4;
-  canvas.width = (textBoxWidth + textBoxOffset) * 2;
-  canvas.style.backgroundColor = "salmon";
-
-
-  // add the background of the label
-  ctx.fillStyle = sBgColor;
-  ctx.fillRect((canvas.width / 2 + textBoxOffset), 0, textBoxWidth, textBoxHeight);
-
-  ctx.fillStyle = sColor;
-
-  // set the font styles again, as now they have reset because of the canvas resizing
-  ctx.font = fontStyles;
-
-  ctx.fillText(sText, (canvas.width / 2 + textBoxOffset + textBoxPadding), (fontSize + textBoxPadding));
-
-
-  // draw a line from the bottom-center of the text to all the way down the canvas
-  ctx.fillStyle = 'gray';
-  // draw the underline
-  ctx.beginPath();
-  ctx.moveTo((canvas.width / 2 + textBoxOffset - textBoxPadding), textBoxHeight / 2);
-  ctx.lineTo(canvas.width / 2, canvas.height / 6);
-  ctx.lineTo(canvas.width / 2, canvas.height / 2);
-  ctx.stroke();
-
-  return canvas;
-}
-
-function getObjectLabel( ) {
-
-}
